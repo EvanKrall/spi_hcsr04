af34f0fe59bbdcc5d73fbe7750d42bd9868b5108
Evan Krall <meatmanek@gmail.com>
2017-12-06 16:41:29 -0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+spi_hcsr04: spi_hcsr04.c
+	gcc -std=gnu11 spi_hcsr04.c -o spi_hcsr04
+
+install: spi_hcsr04
+	mv spi_hcsr04 /usr/local/bin/spi_hcsr04
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /sys/kernel/config/device-tree/overlays/spi
+cat /lib/firmware/nextthingco/chip/sample-spi.dtbo > /sys/kernel/config/device-tree/overlays/spi/dtbo
