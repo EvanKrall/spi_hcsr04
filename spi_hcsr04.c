b46d612beb13dce1bea69ac3367567dc3bbafbb8
Evan Krall <meatmanek@gmail.com>
2017-12-06 17:17:31 -0800

    Do the math to calculate distance

--- spi_hcsr04.c
+++ spi_hcsr04.c
@@ -28,9 +28,10 @@ static void pabort(const char *s)
 
 static const char *device = "/dev/spidev32766.0";
 static uint8_t mode;
-static uint32_t speed = 500000;
+static uint32_t clock_speed = 500000;
 static uint32_t num_measurements = 100;
 static size_t buf_size = 256;
+static double speed_of_sound = 343.0; // meters per second.
 
 static int transfer(int fd)
 {
@@ -43,7 +44,7 @@ static int transfer(int fd)
         .rx_buf = (unsigned long)rx,
         .len = buf_size,
         .delay_usecs = 0,
-        .speed_hz = speed,
+        .speed_hz = clock_speed,
         .bits_per_word = 8,
     };
 
@@ -77,8 +78,14 @@ static void print_usage(const char *prog)
     printf("Usage: %s [-DsbdlHOLC3]\n", prog);
     puts("  -D --device            device to use (default /dev/spidev32766.0)\n"
          "  -s --speed             max speed (Hz)\n"
-         "  -n --num-measurements  Number of measurements to take; result is the median of these. (Default 100)\n"
-         "  -b --buf-size          Number of bytes to read; affects maximum distance readable and how\n"
+         "  -n --num-measurements  Number of measurements to take; result is the median of\n"
+         "                         these. (Default 100)\n"
+         "  -b --buf-size          Number of bytes to read; affects maximum distance\n"
+         "                         readable and how much time measurements take.\n"
+         "  -c --speed-of-sound    Speed of sound, in (preferred output units)/second.\n"
+         "                         Default to 343.0, which is the speed of sound in\n"
+         "                         meters/second at 20C. For output in millimeters, use\n"
+         "                         343000; for output in feet, use 1125.\n"
     );
     exit(1);
 }
@@ -91,11 +98,12 @@ static void parse_opts(int argc, char *argv[])
             { "speed",              1, 0, 's' },
             { "num-measurements",   1, 0, 'n' },
             { "buf-size",           1, 0, 'b' },
+            { "speed-of-sound",     1, 0, 'c' },
             { NULL, 0, 0, 0 },
         };
         int c;
 
-        c = getopt_long(argc, argv, "D:s:n:d:b:", lopts, NULL);
+        c = getopt_long(argc, argv, "D:s:n:d:b:c:", lopts, NULL);
 
         if (c == -1)
             break;
@@ -105,7 +113,7 @@ static void parse_opts(int argc, char *argv[])
             device = optarg;
             break;
         case 's':
-            speed = atoi(optarg);
+            clock_speed = atoi(optarg);
             break;
         case 'n':
             num_measurements = atoi(optarg);
@@ -113,13 +121,15 @@ static void parse_opts(int argc, char *argv[])
         case 'b':
             buf_size = atoi(optarg);
             break;
+        case 'c':
+            speed_of_sound = atof(optarg);
+            break;
         default:
             print_usage(argv[0]);
             break;
         }
     }
 }
-
 
 int compare_ints(const void *pa, const void *pb) {
     const int *a = (const int *)pa;
@@ -154,11 +164,11 @@ int main(int argc, char *argv[])
     /*
      * max speed hz
      */
-    ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &clock_speed);
     if (ret == -1)
         pabort("can't set max speed hz");
 
-    ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+    ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &clock_speed);
     if (ret == -1)
         pabort("can't get max speed hz");
 
@@ -170,7 +180,7 @@ int main(int argc, char *argv[])
 
     qsort(measurements, num_measurements, sizeof(measurements[0]), compare_ints);
 
-    printf("%d\n", measurements[num_measurements/2]);
+    printf("%f\n", measurements[num_measurements/2] * speed_of_sound / clock_speed);
     close(fd);
 
     return ret;
